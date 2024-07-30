d42f125f5abc21aa4cb174ae40405a81530d5b83
GoHo <wantdabo@gmail.com>
2024-07-30 19:23:09 +0800

    配置工具提交

---
+++ config/Commands/client_copy.bat
@@ -0,0 +1,2 @@
+xcopy /e/y "%cd%\..\Cfg\Bytes\*" "%cd%\..\..\goblin\Assets\GameRes\Raws\Configs\"
+xcopy /e/y "%cd%\..\Cfg\CS\*" "%cd%\..\..\goblin\Assets\Scripts\Goblin\Common\"
---
+++ config/Commands/server_copy.bat
@@ -0,0 +1,2 @@
+xcopy /e/y "%cd%\..\Cfg\Bytes\*" "%cd%\..\..\queen\Queen\Res\Configs\"
+xcopy /e/y "%cd%\..\Cfg\CS\*" "%cd%\..\..\queen\Queen\Common\"
